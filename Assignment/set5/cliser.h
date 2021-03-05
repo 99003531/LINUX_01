67192e6f7c487b3c2b4ec72f988133f6694e5fe2
99003531 <78536469+99003531@users.noreply.github.com>
2021-03-05 23:39:47 +0530

    Add files via upload

---
+++ Assignment/set5/cliser.h
@@ -0,0 +1,14 @@
+#ifndef CLISER_H_INCLUDED
+#define CLISER_H_INCLUDED
+#include<mqueue.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+
+#endif // SOL2E_H_INCLUDED
